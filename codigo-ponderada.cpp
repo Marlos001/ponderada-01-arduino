d179750576a7b885323879a53d2bd5de87954949
Marlos Guedes <marlos.guedes@sou.inteli.edu.br>
2024-10-15 09:01:26 -0300

    Código da ponderada

---
+++ codigo-ponderada.cpp
@@ -0,0 +1,16 @@
+// C++ code
+// https://www.tinkercad.com/things/4JelX3FAghp/editel?sharecode=w3VzCzQT3PfakXtJR1JUCI1jT9aqmUFQwpD7Cmg_67g
+
+
+void setup()
+{
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
+  delay(1000);
+}
